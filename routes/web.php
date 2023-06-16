2f3495c9b2ecf7bd45718513bc35505c196dad3d
iqbalmusyaffa <55908032+iqbalmusyaffa@users.noreply.github.com>
2023-06-16 10:15:30 +0700

    fixed route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home', [HomeController::class, 'index'])->name('home');
-Route::get('profile', ProfileController::class)->name('profile');
-Route::resource('employees', EmployeeController::class);
+
